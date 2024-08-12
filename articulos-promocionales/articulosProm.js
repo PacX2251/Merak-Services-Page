0ef2d01b208ca706c37aca7e839771635b30c7ee
PacX2251 <pacoroblesmoral@gmail.com>
2024-08-11 22:18:47 -0600

    Actualizacion Catalogo Automatico

--- articulos-promocionales/articulosProm.js
+++ articulos-promocionales/articulosProm.js
@@ -38,59 +38,65 @@ function refreshGrid(catChosen,data){
   for (var prod = 1; prod <= 12; prod++){
     //Tipo de categoria Para definir el primer digito del articulo
     //cambio de pagina conocer la direccion del cambio sumar o restar 12 segun sea el caso
-    var res = catChosen.toString() + "_" + prod.toString()
+    var res = catChosen.toString() + "_" + prod.toString();
     curArt.push(res);
   }
-  //console.log(curArt);
-
-  //var categoria = "";
-  //var producto = "";
-  //var car_1 = "";
-  //var car_2 = "";
-  //var colores = [];
-  //var imagen = [];
 
   var art = 0;
   var aux = 0;
+  var columna = [];
+  var mosContainer = document.getElementById("paquetes__mosaico");
+  
+  //Crear contenido de paquetes mosaico por columna
   for (var i = 1; i <= 4; i++){
-    var mosContainer = document.getElementById("paquetes__mosaico");
+    var colum = '<div class="column">';
+
+    //Crear contenido de columna por fila
+    var fila = [];
     for (var j = 1; j <= 3; j++){
       aux = aux + 1;
+      //Definir si es un paquete s m l
       if(aux===3 || aux===5 || aux===7 || aux===12){
         var size = "paquete s";
       }
       
-      if(aux===1 || aux===6 || aux===8 || aux===10){
+      else if(aux===1 || aux===6 || aux===8 || aux===10){
         var size = "paquete m";
       }
       
-      if(aux===2 || aux===4 || aux===9 || aux===11){
+      else if(aux===2 || aux===4 || aux===9 || aux===11){
         var size = "paquete l";
       }
-      //console.log("data",data[data[curArt[art].toString()].toString()]);
-      //console.log("data",data[curArt[art].toString()]);
-      var inicio = '<div class="column">';
-      var imgn = '<div class="' + size + '"><img src="./art_img/'+ data[curArt[art].toString()].imagen +'" class="paquete__img" alt="Paquete"/> ';
+      
+      var paq = '<div class="' + size;
+      var imgn = '"><img src="./art_img/'+ data[curArt[art].toString()].imagen +'" class="paquete__img" alt="Paquete"/> ';
+      var over = '<div class="overlay"> <div class="cont_over">';
+      //CIRULOS DE COLORES
       var linea = '<svg xmlns="http://www.w3.org/2000/svg" width="150" height="2" viewBox="0 0 150 2" fill="none"> <path d="M2 2L150 2" stroke="#E6C067" stroke-width="3" /> </svg>';
-      var cont = '<h3>'+ data[curArt[art].toString()].producto +'</h3> <p> '+ data[curArt[art].toString()].car_1 +' <br />'+ data[curArt[art].toString()].car_2 +'</p>';
+      var cont = '<h3>'+ data[curArt[art].toString()].producto +'</h3> <p> '+ data[curArt[art].toString()].car_1 +' <br />'+ data[curArt[art].toString()].car_2 +'</p> </div> </div> </div>'; //divs del paquete 
+      fila[j] = paq + imgn + over + linea + cont;
+      
       
       var numCol = data[curArt[art]].colores.length;
-      console.log(data[curArt[art]].producto,": ",numCol);
+      //console.log(data[curArt[art]].producto,": ",numCol);
 
       var colArray = [];
       for (var k = 0; k < numCol; k++){
         colArray.push(data[curArt[art]].colores[k].length);
       }
 
+      if (colArray[k] > 1){
+
+      }
       console.log(colArray);
-    
-      //CIRULOS DE COLORES
-      var fin = '</div> </div> </div> </div>';
-      var conjunto = inicio + imgn + linea + cont + fin;
+
       art = art + 1;
-      //console.log(art);
     }
-    mosContainer.innerHTML = conjunto;
+    var finColum = '</div>'; //column paquetes__mosaico
+    columna[i] = colum + fila[1] + fila[2] + fila[3] + finColum;
   }
+
+  conFinal = columna[1] + columna[2] + columna[3] + columna[4];
+  mosContainer.innerHTML = conFinal;
   art = 0;
 }
